d570e11bec563d1972e4ddf12c260c78cd1aa578
Lacey R <laceyrice@me.com>
2015-09-30 22:48:09 -0400

    some working tests

--- js/tests.js
+++ js/tests.js
@@ -1,7 +1,7 @@
 /***
 Get to testing!
-    * [ ] What kind of thing is `game.board`...? What does it do?
-    * [ ] How about `game.reset`? What does it _return_? Any side-effects?
+    * [x] What kind of thing is `game.board`...? What does it do?
+    * [x] How about `game.reset`? What does it _return_? Any side-effects?
     * [ ] Resetting a board with no moves is lame, HOW U EVEN `game.applyMove`?
     * [ ] First move is free, next one costs $10.
     * [ ] Just calling `game.applyMove` is cheating; `game.next`...!
@@ -12,28 +12,26 @@ Get to testing!
   * [ ] Wipe the tracer bullets out before you push!
 
 ***/
+(function(globals){
 
-var test = require('mocha').it,
-    expect = require('chai').expect, // Use `expect(X).to.be` et al
-    assert = require('chai').assert;  // OR use `assert.equal` etc
 
-/* example
-    test('getting a list of multiples of 3', function () {
-      expect(ten()).to.deep.equal([3, 6, 9]);
-      expect(twenty()).to.deep.equal([3, 6, 9, 12, 15, 18]);
-      expect(fifty()).to.deep.equal([3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48]);
-    });
-*/
-test('what is game.board', function () {
-  expect(game.board(1)).to.be.(["P"]); //game is the name of the object that board is a property of
+it('should be a function', function(){
+  chai.expect(game.board).to.be.a('function'); //game is the name of the object that board is a property of
 
-}
-)
+});
 
-var game = globals.game = {
+it('should return an array', function(){
+  chai.expect(game.board()).to.be.an('array');
+});
 
-board: function(){
-  return board.map(function(row){
-    return row.slice();
-  });
-}
+it('should be a function', function(){
+  chai.expect(game.reset).to.be.a('function');
+});
+
+it('should return an object', function(){
+  chai.expect(game.reset()).to.be.an('object');
+});
+
+
+
+})(window || module && module.exports || this);
